14580116b00c8ce47ee3f4e432241da1541242e7
nehavenkat <neha.veketsetty@gmail.com>
2019-12-13 14:16:48 +0100

    reviews

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const server = express()
 const cors = require("cors")
+const reviewProducts = require("./src/reviews/index.js") //12) we import our reviews router
 server.use(express.json())
 server.use(cors())
 const port = 4000
@@ -8,3 +9,4 @@ const port = 4000
 server.listen(port, () => {
     console.log(`We are running on localhost ${port}`)
 })
+server.use("/reviews", reviewProducts) // 13)we assign the reviewRouter on /review // 14) check it on POSTMAN GET http://localhost:4000/reviews
